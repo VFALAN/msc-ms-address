65625948b65afdb82adf89a6092457e7ea896e1b
VFALAN <alanvillafan95@gmail.com>
2024-02-22 11:29:48 -0600

    first commit

---
+++ src/main/java/com/msc/ms/address/MscMsAddressApplication.java
@@ -0,0 +1,13 @@
+package com.msc.ms.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscMsAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscMsAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/ms/address/MscMsAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.ms.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscMsAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
